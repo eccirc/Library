0d13d8194cd7d39ae3788974f353aaa06c2e8c5c
= <=>
2022-04-20 11:41:16 +0100

    Add Readme and UML diagram png

---
+++ README.md
@@ -0,0 +1,35 @@
+# Virtual Library - !!WIP!!
+**Author** David Williams 
+
+**Date** April 2022
+
+**Last Updated** April 2022
+
+## About 
+
+An interactive command line application written in Java which seeks to implement the following:
+- View a list of available books
+- Basic data persistence with the ability to read/write JSON files
+- Read raw data from a CSV data file
+- Loan system - a user can borrow and return books
+- Joining - a user can join as a new user (with persistent a record)
+- Reporting - an admin class user can run a report to check the status of library items and output this in a CSV file
+
+
+## Technologies / dependencies
+
+- **Java**
+- **Maven** 
+- **openCSV** 
+- **Jackson**
+
+## Credits / Attributions
+
+****
+
+
+## Screenshots
+
+### UML
+
+![UML diagram](./LibraryUML.png)
--- src/main/java/com/postcourse/Library.java
+++ src/main/java/com/postcourse/Library.java
@@ -8,21 +8,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
-
-//
-//        A functioning Library System: You will build a console application that allows the user to
-//        see what books (data provided) the library currently has available. The data will be
-//        provided in CSV format, parse this into JSON format and write the data to a JSON file.
-//        This should be done using Java and not done manually. The idea is to add persistence
-//        to your application by reading/writing to a file.
-//
-//        Loan System: A user should be able to loan a book and also see what books they
-//        currently have loaned. A user should not be able to loan a book that is already out on
-//        loan. Write to a new JSON file to make the users persistent.2
-//        3. Reports: A library admin should be able to run a report that shows all the books
-//        currently out on loan and also the amount of times a particular books have been
-//        loaned out. Ext. Can you output the report in CSV format?
-
 public class Library {
 
     private final Map<ItemType,List<LibraryItem>> items = Map.of(ItemType.BOOK, Inventory.getBooksFromJSON());
